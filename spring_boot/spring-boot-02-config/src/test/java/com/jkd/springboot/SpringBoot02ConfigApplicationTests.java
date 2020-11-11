a56fd8c916e6a9f36c5ec04b7b22423fa7bd8e19
姬阔东 <jikuodong@froeasy.net>
2020-11-11 17:42:15 +0800

    配置文件值注入

--- spring_boot/spring-boot-02-config/src/test/java/com/jkd/springboot/SpringBoot02ConfigApplicationTests.java
+++ spring_boot/spring-boot-02-config/src/test/java/com/jkd/springboot/SpringBoot02ConfigApplicationTests.java
@@ -1,13 +1,24 @@
 package com.jkd.springboot;
 
+import com.jkd.springboot.bean.Person;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+/**
+ * SpringBoot单元测试
+ *
+ * 可以在测试期间很方便的类似编码一样进行自动注入等容器的功能
+ */
 @SpringBootTest
 class SpringBoot02ConfigApplicationTests {
 
+    @Autowired
+    Person person;
     @Test
     void contextLoads() {
+        System.out.println(person);
     }
 
 }
