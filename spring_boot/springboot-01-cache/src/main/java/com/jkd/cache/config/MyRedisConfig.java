a36ac903ec60facdb0300c5a0605816c6e0da937
姬阔东 <jikuodong@froeasy.net>
2021-01-08 15:24:46 +0800

    springboot 整合redis缓存数据

---
+++ spring_boot/springboot-01-cache/src/main/java/com/jkd/cache/config/MyRedisConfig.java
@@ -0,0 +1,37 @@
+package com.jkd.cache.config;
+
+import com.jkd.cache.bean.Employee;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+import java.time.Duration;
+
+@Configuration
+public class MyRedisConfig {
+
+    @Bean
+    public RedisTemplate<Object, Employee> empRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<Object, Employee> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
+        Jackson2JsonRedisSerializer<Employee> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(Employee.class);
+        template.setDefaultSerializer(jackson2JsonRedisSerializer);
+        return template;
+    }
+    @Bean
+    public RedisCacheManager employeeRedisCacheManager(RedisConnectionFactory redisConnectionFactory) {
+        RedisCacheConfiguration cacheConfiguration =
+                RedisCacheConfiguration.defaultCacheConfig()
+                        .entryTtl(Duration.ofDays(1))   // 设置缓存过期时间为一天
+                        .disableCachingNullValues()     // 禁用缓存空值，不缓存null校验
+                        .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new
+                                GenericJackson2JsonRedisSerializer()));     // 设置CacheManager的值序列化方式为json序列化，可加入@Class属性
+        return RedisCacheManager.builder(redisConnectionFactory).cacheDefaults(cacheConfiguration).build();     // 设置默认的cache组件
+    }
+}
