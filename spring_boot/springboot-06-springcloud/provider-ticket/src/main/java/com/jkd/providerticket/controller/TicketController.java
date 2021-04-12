c526a485ab9d791c43bdae9a3ff89402db22e62c
姬阔东 <jikuodong@froeasy.net>
2021-04-12 11:52:09 +0800

    springboot+ springCloud的基础测试

---
+++ spring_boot/springboot-06-springcloud/provider-ticket/src/main/java/com/jkd/providerticket/controller/TicketController.java
@@ -0,0 +1,21 @@
+package com.jkd.providerticket.controller;
+
+import com.jkd.providerticket.service.TicketService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ */
+@RestController
+public class TicketController {
+
+    @Autowired
+    TicketService ticketService;
+
+    @GetMapping("/ticket")
+    public String getTicket() {
+        return ticketService.getTicket();
+    }
+}
---
+++ spring_boot/springboot-06-springcloud/provider-ticket/src/main/java/com/jkd/providerticket/service/TicketService.java
@@ -0,0 +1,14 @@
+package com.jkd.providerticket.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ */
+@Service
+public class TicketService {
+
+    public String getTicket() {
+        return "《厉害了，我的国》";
+    }
+}
