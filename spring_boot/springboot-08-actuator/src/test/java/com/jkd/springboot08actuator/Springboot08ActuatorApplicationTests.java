8d25cc99181131254ef55cabe23f1e5dc66a6df8
姬阔东 <jikuodong@froeasy.net>
2021-04-16 14:40:06 +0800

    Springboot + actuator的基础整合使用

---
+++ spring_boot/springboot-08-actuator/src/test/java/com/jkd/springboot08actuator/Springboot08ActuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jkd.springboot08actuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot08ActuatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
